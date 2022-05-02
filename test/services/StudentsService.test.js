ee6f626677c3e469133f94df073998fde51fe56c
UsielOM <usiel31197@outlook.es>
2022-05-02 16:59:03 -0500

    Creando la primera prueba para obtener los datos de los estudiantes

---
+++ test/services/StudentsService.test.js
@@ -0,0 +1,8 @@
+const StudentsService = require("../../lib/services/StudentsService");
+describe("Test para Students Service", () => {
+    test("Requerimiento 1: consultar todos los estudiantes con todos sus campos", () => {
+        const estudiante = [{ id: "6264d5d89f1df827eb84bb23", name: "Warren", email: "Todd@visualpartnership.xyz", creditS: 500 }]
+        const getStudent = StudentsService.getStudentsAll();
+        expect(getStudent).toBe(estudiante);
+    });
+});

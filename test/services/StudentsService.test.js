560b4c3319ab349bfc961a0a94da3630a73be9c2
UsielOM <usiel31197@outlook.es>
2022-05-02 17:10:23 -0500

    Haciendo mis pruebas para obetner los estudaiantes

--- test/services/StudentsService.test.js
+++ test/services/StudentsService.test.js
@@ -2,7 +2,7 @@ const StudentsService = require("../../lib/services/StudentsService");
 describe("Test para Students Service", () => {
     test("Requerimiento 1: consultar todos los estudiantes con todos sus campos", () => {
         const estudiante = [{ id: "6264d5d89f1df827eb84bb23", name: "Warren", email: "Todd@visualpartnership.xyz", creditS: 500 }]
-        const getStudent = StudentsService.getStudentsAll();
+        const getStudent = StudentsService.getStudentsAll(estudiante);
         expect(getStudent).toBe(estudiante);
     });
 });
